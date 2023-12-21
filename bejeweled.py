e79051dbe1f1bdbc52194d63bdd87ca9b1458e71
ezekielulrich <ezekielulrich@gmail.com>
2023-12-21 11:39:50 -0500

    Comments

--- bejeweled.py
+++ bejeweled.py
@@ -1,11 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# Variables
+url = 'https://www.bubbleshooter.net/game/bejeweled/'
+
+# Create the driver to interact with the web version of Bejeweled
 driver = webdriver.Firefox()
 
-driver.get('https://www.bubbleshooter.net/game/bejeweled/')
+# Open specified page
+driver.get(url)
 
+# Navigate to and click play button
 playButton = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div[2]/a/div")
 playButton.click()
 
-# driver.quit()
+# Use computer vision to interact from here on
+
+driver.quit()
