77e2a39a8f8eecbde568f728992a43b52ef2764f
ezekielulrich <ezekielulrich@gmail.com>
2023-12-21 13:14:56 -0500

    Mute new

--- bejeweled.py
+++ bejeweled.py
@@ -1,14 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.firefox.options import Options
 
 # Variables
 url = 'https://www.bubbleshooter.net/game/bejeweled/'
 
-# Functions
-
-# Create the driver to interact with the web version of Bejeweled
-driver = webdriver.Firefox()
+# Create the mute driver to interact with the web version of Bejeweled
+options = Options()
+options.set_preference("media.volume_scale", "0.0")
+driver = webdriver.Firefox(options=options)
 
 # Open specified page
 driver.get(url)
@@ -16,10 +17,10 @@ driver.get(url)
 # Navigate to and click play button
 playButton = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/div[2]/a/div")
 actions = ActionChains(driver)
-actions.move_to_element(playButton).click()
+actions.move_to_element(playButton).click().perform()
 
-# Allow time for game to load
-actions.pause(5)
-
+# Go fullscreen
+fullscreen = driver.find_element(By.XPATH, '//*[@id="toggle_fullscreen"]')
+actions.click(fullscreen).perform()
 
 driver.quit()
